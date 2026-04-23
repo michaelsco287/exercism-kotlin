2973869bcc7f2b924971bb50cc074015680be019
michaelsco287 <22979445+michaelsco287@users.noreply.github.com>
2026-04-23 11:34:29 +0100

    collatzcalculator

--- collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,5 +1,20 @@
 object CollatzCalculator {
     fun computeStepCount(start: Int): Int {
-        TODO("Implement the function to complete the task")
+        require(start >= 1) { "Invalid start: $start" }
+        var num = start
+        var steps = 0
+        while (num != 1){
+            num = computeNext(num)
+            steps += 1
+        }
+        return steps
+    }
+
+    fun computeNext(num: Int): Int {
+        return if (num % 2 == 0){
+            num / 2
+        } else {
+            num * 3 + 1
+        }
     }
 }
