2686150886f9db82d35d975f5d7f6b2f9861e98c
michaelsco287 <22979445+michaelsco287@users.noreply.github.com>
2026-04-23 12:08:54 +0100

    finished darts

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,16 @@
+import kotlin.math.sqrt
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    fun score(x: Number, y: Number): Int {
+        val xSquared = x.toDouble() * x.toDouble()
+        val ySquared = y.toDouble() * y.toDouble()
+        val radius = sqrt(xSquared + ySquared)
+        return when {
+            radius <=  1 -> 10
+            radius <= 5 -> 5
+            radius <= 10 -> 1
+            else -> 0
+        }
     }
 }
