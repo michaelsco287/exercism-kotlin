860e0728a142dbbd458f4976322dd65e6eb0a08b
michaelsco287 <22979445+michaelsco287@users.noreply.github.com>
2026-04-23 14:21:44 +0100

    changed etl

--- etl/src/main/kotlin/ETL.kt
+++ etl/src/main/kotlin/ETL.kt
@@ -1,12 +1,10 @@
 object ETL {
-    fun transform(source: Map<Int, Collection<Char>>): Map<Char, Int> {
-        val output = mutableMapOf<Char, Int>()
-        for((score, characters) in source ){
+    fun transform(source: Map<Int, Collection<Char>>): Map<Char, Int> = buildMap{
+        for((score, characters) in source){
             for (char in characters) {
                 val key: Char = char.lowercaseChar()
-                output[key] = score
+                this[key] = score
             }
         }
-        return output
     }
 }
