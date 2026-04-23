551e8934481a53f77dc38dc0e8de6b699ec3d837
michaelsco287 <22979445+michaelsco287@users.noreply.github.com>
2026-04-23 14:43:31 +0100

    first solution

--- grains/src/main/kotlin/Grains.kt
+++ grains/src/main/kotlin/Grains.kt
@@ -3,10 +3,15 @@ import java.math.BigInteger
 object Board {
 
     fun getGrainCountForSquare(number: Int): BigInteger {
-        TODO("Implement this function to complete the task")
+        require((number >= 1) and (number <= 64)) { "Invalid square: $number" }
+        return 2.toBigInteger().pow(number-1)
     }
 
     fun getTotalGrainCount(): BigInteger {
-        TODO("Implement this function to complete the task")
+        var total: BigInteger = BigInteger.ZERO
+        for (num in 1..64){
+            total += getGrainCountForSquare(num)
+        }
+        return total
     }
 }
