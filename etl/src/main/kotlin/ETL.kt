d81463f57711d8fe49191c4d35c1d4664b3cd4ab
michaelsco287 <22979445+michaelsco287@users.noreply.github.com>
2026-04-23 14:14:30 +0100

    etl finish

--- etl/src/main/kotlin/ETL.kt
+++ etl/src/main/kotlin/ETL.kt
@@ -1,5 +1,12 @@
 object ETL {
     fun transform(source: Map<Int, Collection<Char>>): Map<Char, Int> {
-        TODO("Implement the function to complete the task")
+        val output = mutableMapOf<Char, Int>()
+        for((score, characters) in source ){
+            for (char in characters) {
+                val key: Char = char.lowercaseChar()
+                output[key] = score
+            }
+        }
+        return output
     }
 }
