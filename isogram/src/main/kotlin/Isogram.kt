b3a34247fa3eae391bf1f55adb8c884a8256bd8f
michaelsco287 <22979445+michaelsco287@users.noreply.github.com>
2026-04-29 12:14:15 +0100

    finish isogram

--- isogram/src/main/kotlin/Isogram.kt
+++ isogram/src/main/kotlin/Isogram.kt
@@ -1,6 +1,12 @@
 object Isogram {
 
     fun isIsogram(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val letters: MutableSet<Char> = mutableSetOf()
+        for(i in input.lowercase()){
+            if (i.isLetter() && !letters.add(i)){
+                return false
+            }
+        }
+        return true
     }
 }
