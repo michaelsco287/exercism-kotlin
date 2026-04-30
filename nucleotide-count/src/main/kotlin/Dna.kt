55f25bf60cdf91092548d03dfc6f61a7cffd34cd
michaelsco287 <22979445+michaelsco287@users.noreply.github.com>
2026-04-30 16:21:54 +0100

    4 tests passing nucleotide

--- nucleotide-count/src/main/kotlin/Dna.kt
+++ nucleotide-count/src/main/kotlin/Dna.kt
@@ -1,9 +1,14 @@
-class Dna {
-
-    // TODO: Implement proper constructor
+class Dna(val chain: String) {
 
     val nucleotideCounts: Map<Char, Int>
         get() {
-            TODO("Implement this function to complete the task")
+            val result: MutableMap<Char, Int> = mutableMapOf('A' to 0, 'C' to 0, 'G' to 0, 'T' to 0)
+            for(i in chain){
+                println("$i in chain: $chain")
+                println("result.keys = ${result.keys}")
+                require(i in result.keys)
+                result.compute(i){_, value -> value?.plus(1) }
+            }
+            return result
         }
 }
